f93a8e6fb1f054033d443567ffd17e43440e2a86
Zachary Sturman <zasturman@gmail.com>
2025-12-09 06:49:58 -0800

    Update to impage optimizer

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Ensure packages that need transpilation are handled correctly
+  // Required for next-image-export-optimizer (since v1.2.0)
+  // See package docs if you add more packages that need transpiling.
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore-next-line -- NextConfig typing may not include transpilePackages yet
+  transpilePackages: ["next-image-export-optimizer"],
   images: {
     remotePatterns: [
       {
