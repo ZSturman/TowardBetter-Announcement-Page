a021543fa8937c86da8f4b81dee7e5b5297a828d
Zachary Sturman <zasturman@gmail.com>
2025-12-09 22:39:35 -0800

    Build goes to docs folder

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Ensure packages that need transpilation are handled correctly
-  // Required for next-image-export-optimizer (since v1.2.0)
-  // See package docs if you add more packages that need transpiling.
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore-next-line -- NextConfig typing may not include transpilePackages yet
+  output: "export",
+  distDir: "docs",
+  trailingSlash: true,
+  basePath: "/TowardBetter-Announcement-Page",
+  assetPrefix: "/TowardBetter-Announcement-Page/",
   transpilePackages: ["next-image-export-optimizer"],
+    env: {
+    nextImageExportOptimizer_imageFolderPath: "public/images",
+    nextImageExportOptimizer_exportFolderPath: "docs",
+    nextImageExportOptimizer_quality: "75",
+    nextImageExportOptimizer_storePicturesInWEBP: "true",
+    nextImageExportOptimizer_exportFolderName: "static",
+    nextImageExportOptimizer_generateAndUseBlurImages: "true",
+    nextImageExportOptimizer_remoteImageCacheTTL: "0",
+  },
   images: {
+        loader: "custom",
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     remotePatterns: [
       {
         protocol: 'https',
@@ -20,3 +32,5 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+
