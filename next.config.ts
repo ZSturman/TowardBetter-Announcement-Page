63c26bd5cd1ec479a06876bed6ab422bbd7792f8
Zachary Sturman <zasturman@gmail.com>
2025-12-09 22:52:18 -0800

    Image optimizer configured

--- next.config.ts
+++ next.config.ts
@@ -17,9 +17,6 @@ const nextConfig: NextConfig = {
     nextImageExportOptimizer_remoteImageCacheTTL: "0",
   },
   images: {
-        loader: "custom",
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     remotePatterns: [
       {
         protocol: 'https',
